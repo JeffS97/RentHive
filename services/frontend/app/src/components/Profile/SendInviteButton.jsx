7e234645419992582a23a25bee2812a7442ab31f
Eunice Chew <eunice.chew.2021@scis.smu.edu.sg>
2023-03-30 17:55:07 +0800

    Hovers for the send invite button

--- services/frontend/app/src/components/Profile/SendInviteButton.jsx
+++ services/frontend/app/src/components/Profile/SendInviteButton.jsx
@@ -1,26 +1,67 @@
-import {BiSend, BiMailSend} from "react-icons/bi";
+import {BiSend, BiMailSend, BiNoEntry} from "react-icons/bi";
+import {RiMailCheckLine, RiMailCloseLine} from "react-icons/ri";
 import {React, useState} from "react";
 import "./Profile.css";
 
 export default function SendInviteButton() {
     const [isInviteSent, setIsInviteSent] = useState(false);
+    const [hover, setHover] = useState(false);
+
+    function handleClick() {
+        if (isInviteSent == false) {
+            setIsInviteSent(true);
+        } else {
+            setIsInviteSent(false);
+        }
+        
+        console.log("isInviteSent: " + isInviteSent);
+    }
+
+    function handleHover() {
+        setHover(true);
+        console.log("hover: " + hover);
+
+    }
+
+    function handleMouseLeave() {
+        setHover(false);
+        console.log("hover: " + hover);
+
+    }
 
     return (
         <>
             {
                 isInviteSent == false? 
-                <button className="yellow-profile-button-container" onClick={()=>{setIsInviteSent(true)}}>
+                <button className="yellow-profile-button-container" onClick={handleClick}>
                     <BiSend/>
                     <text className="black-subtitle-text">
                         Send Invite
                     </text>
                 </button>
                 :
-                <button className="outlined-profile-button-container">
-                    <BiMailSend/>
-                    <text className="black-subtitle-text">
-                        Invite Sent!
-                    </text>
+                <button className="outlined-profile-button-container" onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
+                    {
+                        hover == false? 
+                        <>
+                            <RiMailCheckLine/>
+                            <text className="black-subtitle-text">
+                                Invite Sent!
+                            </text>
+                        </>
+
+                        :
+
+                        <>
+                            <RiMailCloseLine/>
+                            <text className="black-subtitle-text">
+                                Withdraw?
+                            </text>
+                        </>
+
+                    }
+                    
+                    
                 </button>
             }
         </>
